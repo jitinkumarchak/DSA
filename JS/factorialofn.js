89e13beed8d492df6c90983f825a7e0f9f6e4003
jitinkumarchak <jitin42013@gmail.com>
2026-03-04 23:06:58 +0530

    leapyear

--- JS/factorialofn.js
+++ JS/factorialofn.js
@@ -1,9 +1,12 @@
-function factorailofN(n){
-    let factorial = 1;
-    for(let i =1; i<=n; i++){
-        factorial *= i;
-    }
-    return factorial;
+function factorailofN(n) {
+  if (n < 0) {
+    throw new Error("n should be a -ve int");
+  }
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+  return factorial;
 }
 
-console.log(factorailofN(5));
+console.log(factorailofN("s"));
