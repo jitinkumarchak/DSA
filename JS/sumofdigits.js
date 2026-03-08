b2be524a0d1498a023d328a95fd2cea166447534
jitinkumarchak <jitin42013@gmail.com>
2026-03-08 23:41:04 +0530

    sum

--- JS/sumofdigits.js
+++ JS/sumofdigits.js
@@ -1,11 +1,19 @@
-function sumofDigits(number) {
-  const inputnum = number.toString();
-  const splittednum = inputnum.split("");
-  let sum = 0;
-  splittednum.forEach((num) => {
-    sum = sum + parseInt(num);
-  });
-  return sum;
-}
+// function sumofDigits(number) {
+//   const inputnum = number.toString();
+//   const splittednum = inputnum.split("");
+//   let sum = 0;
+//   splittednum.forEach((num) => {
+//     sum = sum + parseInt(num);
+//   });
+//   return sum;
+// }
 
-console.log(sumofDigits(1234));
+// console.log(sumofDigits(1234));
+
+function sumofDigits(number) {
+  let num = 0;
+  num = number % 10;
+  number = Math.floor(number / 10);
+  return num + sumofDigits(number);
+ }
+ console.log(sumofDigits(1234));
