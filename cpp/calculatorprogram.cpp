f9d7ad18ab04d305b1a0dffcad2eac84aa9e0ffd
jitinkumarchak <jitin42013@gmail.com>
2026-03-12 22:15:15 +0530

    marks-ifelseifelse

---
+++ cpp/calculatorprogram.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 9;
+    int b = 5;
+    char op = '-';
+    int calculate;
+    calculate = a op, b;
+    cout << calculate << endl;
+    return 0;
+}
---
+++ cpp/marks.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cin >> marks;
+
+    if (marks >= 90)
+    {
+        cout << "O";
+    }
+    else if (marks >= 80)
+    {
+        cout << "A";
+    }
+    else if (marks >= 70)
+    {
+        cout << "B";
+    }
+    else if (marks >= 60)
+    {
+        cout << "C";
+    }
+    else if (marks >= 50)
+    {
+        cout << "P";
+    }
+    else
+    {
+        cout << "F";
+    }
+    return 0;
+}
