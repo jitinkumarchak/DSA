40af5451ec0cc71602831e61901724cdaa48bb4a
Jitin Kumar chak <143113044+jitinkumarchak@users.noreply.github.com>
2026-03-15 17:56:42 +0530

    Update practice.cpp

--- cpp/practice.cpp
+++ cpp/practice.cpp
@@ -32,16 +32,5 @@ int main()
     //     i++;
     // }
     // cout << sum;
-
-    int n = 4;
-
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = 1; j <= n; j++)
-        {
-            cout << "* " ;
-        }
-        cout << endl;
-    }
     return 0;
 }
