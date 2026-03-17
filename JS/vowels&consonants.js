7f3b57f67df8ab11b85242b9bf1a46083aa99534
jitinkumarchak <jitin42013@gmail.com>
2026-03-17 22:36:12 +0530

    vowel

--- JS/vowels&consonants.js
+++ JS/vowels&consonants.js
@@ -1,15 +1,19 @@
-function vowelscount(str){
-    let count =0;
-    let arr = ["a","e","i","o","u"];
+function countvowels$consonants(str) {
+  let vowelscount = 0;
+  let consonentscount = 0;
+  let vowels = ("aeiou");
+  let consonants = ("bcdfghjklmnpqrstvwxyz");
+  
 
-    for(let i = 0; i<str.length;i++){
-        for(let j =0;j<arr.length;j++){
-            if(str[i] === arr[j]){
-                count++;
-            }
-        }
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      vowelscount++;
+    } else if (consonants.includes(str[i])) {
+      consonentscount++;
     }
-    return count;
+  }
+
+  return { vowelscount, consonentscount };
 }
 
-console.log(vowelscount("hello"));
+console.log(countvowels$consonants("aeiou"));
