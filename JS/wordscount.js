e4e37427247f38e4047e6dee90ec9d4fdbd04a6b
jitinkumarchak <jitin42013@gmail.com>
2026-03-19 22:20:50 +0530

    wordscount1

--- JS/wordscount.js
+++ JS/wordscount.js
@@ -1,9 +1,7 @@
 function wordscount(sentence) {
-  const words = sentence.split(' ');
-  console.log(words);
-  const nonemptyelements = words.filter((element) => element != ' ');
-  console.log(nonemptyelements);
-  return nonemptyelements.length;
+    const words = sentence.split(" ");
+    const filterwords = words.filter((words) => words !== "");
+    return filterwords.length;
 }
 
-console.log(wordscount("my name  is Jitin"));
+console.log(wordscount("myself is jitin  kumar  chak"));
