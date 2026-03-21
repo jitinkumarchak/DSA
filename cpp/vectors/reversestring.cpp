5a24fdb43c6da9333fdcd4adce95aac1b7b2f8d3
jitinkumarchak <jitin42013@gmail.com>
2026-03-21 17:20:24 +0530

    reversetring:vector

---
+++ cpp/vectors/reversestring.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reversestr(string s){
+    int left =0;
+    int right  = s.length()-1;
+
+    while(left < right){
+        swap(s[left],s[right]);
+        left++;
+        right--;
+    }
+    cout << s;
+}
+
+int main(){
+    vector<string> s = {"hello", "jitin"};
+    reversestr(s[0]);
+    reversestr(s[1]);
+    return 0;
+}
