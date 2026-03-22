3a6431603860b55dcb534507108937ca530137aa
jitinkumarchak <jitin42013@gmail.com>
2026-03-22 22:51:39 +0530

    swaptwonums:js

--- JS/swaptwonumbers.js
+++ JS/swaptwonumbers.js
@@ -1,10 +1,16 @@
-let a =9;
-let b =4;
+let a = 9;
+let b = 4;
 
-console.log(a,b);
+console.log(a, b);
 
-let c =b;
+let c = b; //swap using third variable
 b = a;
 a = c;
 
-console.log(a,b);
+console.log(a, b);
+
+a = a + b; // arithmetic operations
+b = a - b;
+a = a - b;
+
+console.log(a, b);
