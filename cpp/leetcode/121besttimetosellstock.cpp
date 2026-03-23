8788f0e3ebfd9ea327f55acf35a6f8941ceeb7cd
jitinkumarchak <jitin42013@gmail.com>
2026-03-23 11:21:24 +0530

    bestbuystock121LEETCODE

--- cpp/leetcode/121besttimetosellstock.cpp
+++ cpp/leetcode/121besttimetosellstock.cpp
@@ -4,18 +4,20 @@ using namespace std;
 int main()
 {
     int n = 5;
-    int arr[n] = {1,2};
+    int arr[n] = {1, 2};
     int bestbuy = arr[0];
     int maxprofit = 0;
 
     for (int i = 0; i < n; i++)
     {
+        if (arr[i] > bestbuy)
+        {
+
+            maxprofit = max(maxprofit, arr[i] - bestbuy);
+        }
         bestbuy = min(bestbuy, arr[i]);
-        maxprofit = max(maxprofit, arr[i] - bestbuy);
     }
-    if(bestbuy == maxprofit){
-        return 0;
-    }
+
     cout << maxprofit << endl;
     return 0;
 }
