b9f418b4d76b1fe2e4d3f969224420528443f8c7
jitinkumarchak <jitin42013@gmail.com>
2026-03-28 13:18:48 +0530

    DNF ALGO:leetcode75

--- cpp/leetcode/75sortColors.cpp
+++ cpp/leetcode/75sortColors.cpp
@@ -2,37 +2,61 @@
 #include <vector>
 using namespace std;
 
-int freqcount(vector<int> &arr,int n){
-    int count0= 0, count1 = 0, count2 = 0;
+void freqcount(vector<int> &arr, int n)
+{
+    //     int count0= 0, count1 = 0, count2 = 0;
 
-    for(int i =0 ; i<n;i++){
-        if(arr[i] == 0) count0++;
-        else if (arr[i] == 1) count1++;
-        else count2++;
-    }
+    //     for(int i =0 ; i<n;i++){
+    //         if(arr[i] == 0) count0++;
+    //         else if (arr[i] == 1) count1++;
+    //         else count2++;
+    //     }
 
-    int idx = 0;
-    for(int i=0;i<count0;i++){
-     arr[idx++] = 0;
-    }
-    for (int i = 0; i < count1; i++)
+    //     int idx = 0;
+    //     for(int i=0;i<count0;i++){
+    //      arr[idx++] = 0;
+    //     }
+    //     for (int i = 0; i < count1; i++)
+    //     {
+    //         arr[idx++] = 1;
+    //     }
+    //     for (int i = 0; i < count2; i++)
+    //     {
+    //         arr[idx++] = 2;
+    //     }
+    // return idx;
+    int mid = 0, low = 0, high = n - 1;
+
+    while (mid <= high)
     {
-        arr[idx++] = 1;
+        if (arr[mid] == 0)
+        {
+            swap(arr[mid], arr[low]);
+            low++;
+            mid++;
+        }
+        else if (arr[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(arr[high], arr[mid]);
+            high--;
+        }
     }
-    for (int i = 0; i < count2; i++)
-    {
-        arr[idx++] = 2;
+    for(int i= 0;i<n;i++){
+        cout << arr[i] << " ";
     }
-return idx;
 }
 
-int main (){
+int main()
+{
 
-
-    vector<int> arr = {0,1,2,0,2,1};
+    vector<int> arr = {0, 1, 2, 0, 2, 1};
     int n = arr.size();
 
-    cout << freqcount(arr,n) << " ";
+    freqcount(arr, n);
 
     return 0;
 }
