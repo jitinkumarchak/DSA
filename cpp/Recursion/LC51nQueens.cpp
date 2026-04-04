e9ac3775738c6d05ff9492fb3206203a0178420d
jitinkumarchak <jitin42013@gmail.com>
2026-04-04 14:09:55 +0530

    LC51:nQueens

---
+++ cpp/Recursion/LC51nQueens.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(vector<string> &board, int row, int col, int n)
+{
+    // horizontal
+    for (int j = 0; j < n; j++)
+    {
+        if (board[row][j] == 'Q')
+        {
+            return false;
+        }
+    }
+
+    // vertical
+
+    for (int i = 0; i < n; i++)
+    {
+        if (board[i][col] == 'Q')
+        {
+            return false;
+        }
+    }
+
+    for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+    }
+
+    for (int i = row, j = col; i >= 0 && j < n; i--, j++)
+    {
+        if (board[i][j] == 'Q')
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void nQueens(vector<string> &board, int row, int n, vector<vector<string>> &ans)
+{
+    if (row == n)
+    {
+        ans.push_back({board});
+        return;
+    }
+
+    for (int j = 0; j < n; j++)
+    {
+        if (isSafe(board, row, j, n))
+        {
+            board[row][j] = 'Q';
+            nQueens(board, row + 1, n, ans);
+            board[row][j] = '.';
+        }
+    }
+}
+
+int main()
+{
+    int n = 4;
+    vector<string> board(n, string(n, '.'));
+    vector<vector<string>> ans;
+
+    nQueens(board, 0, n, ans);
+
+   for(int i = 0; i <n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
