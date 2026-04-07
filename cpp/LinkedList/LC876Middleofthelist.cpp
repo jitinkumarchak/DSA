0a10a242287a6aed320432f34bb1dc01d2cd8ad1
jitinkumarchak <jitin42013@gmail.com>
2026-04-07 22:11:02 +0530

    LC:876 middle of the list

---
+++ cpp/LinkedList/LC876Middleofthelist.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+
+    ListNode(int x)
+    {
+        val = x;
+        next = NULL;
+    }
+};
+
+ListNode *MiddleNode(ListNode *head)
+{
+
+    ListNode *slow = head;
+    ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    return slow;
+}
+
+void printList(ListNode *head)
+{
+    while (head != NULL)
+    {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    // create linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode *head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = MiddleNode(head);
+
+    cout << "Middle node :  ";
+    printList(head);
+
+    return 0;
+}
