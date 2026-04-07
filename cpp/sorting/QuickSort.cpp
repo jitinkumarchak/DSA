dd4b897a53ed39414d1b78a9e18014ee82aa8f33
jitinkumarchak <jitin42013@gmail.com>
2026-04-07 12:27:05 +0530

    Quick Sort

--- cpp/sorting/QuickSort.cpp
+++ cpp/sorting/QuickSort.cpp
@@ -2,8 +2,42 @@
 #include <vector>
 using namespace std;
 
+int partition(vector<int> &nums, int st, int end)
+{
+    int idx = st - 1, pivot = nums[end];
+
+    for (int j = st; j < end; j++)
+    {
+        if (nums[j] <= pivot)
+        {
+            idx++;
+            swap(nums[j], nums[idx]);
+        }
+    }
+
+    idx++;
+    swap(nums[end], nums[idx]);
+    return idx;
+}
+
+void QuickSort(vector<int> &nums, int st, int end)
+{
+    if (st < end)
+    {
+        int pvtidx = partition(nums, st, end);
+        QuickSort(nums, st, pvtidx - 1);  // left half
+        QuickSort(nums, pvtidx + 1, end); // right half
+    }
+}
+
 int main()
 {
     vector<int> nums = {2, 1, 6, 5, 4, 3};
+    QuickSort(nums, 0, nums.size() - 1);
+
+    for(int val : nums){
+        cout  << val  << " ";
+    }
+    cout << endl;
     return 0;
 }
