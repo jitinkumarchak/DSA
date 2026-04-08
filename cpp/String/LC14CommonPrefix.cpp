b49afba56272567fe62c059431939c322652c39f
jitinkumarchak <jitin42013@gmail.com>
2026-04-08 23:32:25 +0530

    LC:14 Common Prefix

---
+++ cpp/String/LC14CommonPrefix.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &strs)
+{
+
+    string prefix = strs[0];
+
+    for (int i = 1; i < strs.size(); i++)
+    {
+
+        while (strs[i].find(prefix) != 0)
+        {
+            prefix = prefix.substr(0, prefix.length() - 1);
+
+            if (prefix == "")
+                return "";
+        }
+    }
+
+    return prefix;
+}
+
+int main()
+{
+
+    vector<string> strs = {"flower", "flow", "flight"};
+
+    cout << longestCommonPrefix(strs);
+
+    return 0;
+}
