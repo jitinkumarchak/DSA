4d55c87312f4d72edd94b242f4f3511c05ef1a33
jitinkumarchak <jitin42013@gmail.com>
2026-04-10 16:51:36 +0530

    Stock Span Problem

--- cpp/Stack/stockspanproblem.cpp
+++ cpp/Stack/stockspanproblem.cpp
@@ -3,27 +3,37 @@
 #include <stack>
 using namespace std;
 
-int main(){
-    vector<int> price = {100,80,60,70,60,75,85};
-    vector<int> ans(price.size(),0);
+// Time_complexity = O(n);
+//  Space_Complexity = O(n);
+
+int main()
+{
+    vector<int> price = {100, 80, 60, 70, 60, 75, 85};
+    vector<int> ans(price.size(), 0);
     stack<int> s;
 
-    for(int i = 0; i< price.size() ; i++){  // O(n) 
-        while(s.size() > 0 && price[s.top()] <= price[i]){
+    for (int i = 0; i < price.size(); i++)
+    { // O(n)
+        while (s.size() > 0 && price[s.top()] <= price[i])
+        {
             s.pop();
         }
 
-        if(s.empty()){
-            ans[i] = i+1;
-        }else{
+        if (s.empty())
+        {
+            ans[i] = i + 1;
+        }
+        else
+        {
             ans[i] = i - s.top(); // i -prevhigh
         }
         s.push(i);
     }
 
-    //print ans vals
+    // print ans vals
 
-    for(int val :ans){
+    for (int val : ans)
+    {
         cout << val << " ";
     }
     cout << endl;
