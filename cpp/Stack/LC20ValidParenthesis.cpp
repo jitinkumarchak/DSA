bc95fda22e5423fc2c2cfbd50e640fcf3d73a7a8
jitinkumarchak <jitin42013@gmail.com>
2026-04-10 15:35:57 +0530

    LC:20 Valid Parenthesis

---
+++ cpp/Stack/LC20ValidParenthesis.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isvalid(string str)
+{
+    stack<char> s;
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[')
+        {
+            s.push(str[i]);
+        }
+        else
+        {
+            if (s.size() == 0)
+            {
+                return false;
+            }
+
+            if (s.top() == '(' && str[i] == ')' || s.top() == '{' && str[i] == '}' || s.top() == '[' && str[i] == ']')
+            {
+                s.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return s.size() == 0;
+}
+
+int main()
+{
+    cout << isvalid("{(}");
+
+    return 0;
+}
