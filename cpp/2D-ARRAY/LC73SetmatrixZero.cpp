c1b702ce2808ee0b17919071e397285f3267b43f
jitinkumarchak <jitin42013@gmail.com>
2026-04-11 22:14:53 +0530

    LC:73 Set MatRix Zeroes

---
+++ cpp/2D-ARRAY/LC73SetmatrixZero.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> setZeroes(vector<vector<int>> &matrix, int n, int m)
+{
+    if (n == 0 || m == 0)
+    {
+        return matrix;
+    }
+
+    int col0 = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                matrix[i][0] = 0;
+                if (j != 0)
+                {
+                    matrix[0][j] = 0;
+                }
+                else
+                {
+                    col0 = 0;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    if (matrix[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[0][j] = 0;
+        }
+    }
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = 0;
+        }
+    }
+    return matrix;
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{0, 1, 1, 0}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+    int n = matrix.size(), m = matrix[0].size();
+    setZeroes(matrix, n, m);
+
+    for (auto &val : matrix)
+    {
+        for (auto &num : val)
+        {
+            cout << num << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
