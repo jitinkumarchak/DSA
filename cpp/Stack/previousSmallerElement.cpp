a13ff2ff0805f7862bdf995705c878b9f3bee891
jitinkumarchak <jitin42013@gmail.com>
2026-04-11 13:00:37 +0530

    previous Smaller Element

---
+++ cpp/Stack/previousSmallerElement.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> previoussmallerelement(vector<int> &arr)
+{
+    vector<int> ans(arr.size(), 0);
+    stack<int> s;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        while (s.size() > 0 && s.top() >= arr[i])
+        {
+            s.pop();
+        }
+
+        if (s.empty())
+        {
+            ans[i] = -1;
+        }
+        else
+        {
+            ans[i] = s.top();
+        }
+
+        s.push(arr[i]);
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 3, 5, 6, 7};
+    vector<int> ans = previoussmallerelement(arr);
+    for (int val : ans)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
