e03ab0a9bed6d6742ce8f78ddd60e6c8ec00049a
jitinkumarchak <jitin42013@gmail.com>
2026-04-13 23:18:37 +0530

    LC:26 Remove Duplicate from Sorted arr

---
+++ cpp/array/LC26RemoveDuplicatesFromSortedArr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int removeduplicates(vector<int> & nums){
+
+    int i = 0;
+    for(int j =1;j<nums.size();j++){
+        if(nums[i] != nums[j]){
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i+1;
+}
+
+
+int main (){
+
+    vector<int> nums = {1,1,2,3,4,5,5};
+   cout <<  removeduplicates(nums);
+    return 0;
+}
