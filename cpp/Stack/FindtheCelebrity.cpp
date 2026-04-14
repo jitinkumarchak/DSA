4ad443f1bfee4cbff6ac3b1f29ee7f484b763466
jitinkumarchak <jitin42013@gmail.com>
2026-04-15 00:20:41 +0530

    LC Find the Celebrity

---
+++ cpp/Stack/FindtheCelebrity.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int celebrity(vector<vector<int>> &arr) // O(n);
+{
+    int n = arr.size();
+    stack<int> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        s.push(i);
+    }
+
+    while (s.size() > 1)
+    {
+        int i = s.top();
+        s.pop();
+
+        int j = s.top();
+        s.pop();
+
+        if (arr[i][j] == 0)
+        {
+            s.push(i);
+        }
+        else
+        {
+            s.push(j);
+        }
+    }
+
+    int celeb = s.top();
+    for (int i = 0; i < n; i++)
+    {
+        if ((i != celeb) && arr[i][celeb] == 0 || arr[celeb][i] == 1)
+        {
+            return -1;
+        }
+    }
+    return celeb;
+}
+
+int main()
+{
+    vector<vector<int>> arr = {{0, 1, 0}, {0, 0, 0}, {0, 1, 0}};
+    cout << celebrity(arr);
+    return 0;
+}
