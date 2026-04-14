3b130f41349cfba002d7b91a511463bdb91e9e94
jitinkumarchak <jitin42013@gmail.com>
2026-04-14 23:51:51 +0530

    LC:42 Trapping Rain Water Two pointer approach

--- cpp/array/LC42TrappingRainWater.cpp
+++ cpp/array/LC42TrappingRainWater.cpp
@@ -15,15 +15,14 @@ int trap(vector<int> &heights)
     // lmax
     for (int i = 1; i < n; i++)
     {
-        lmax[i] = max(lmax[i-1], heights[i]);
+        lmax[i] = max(lmax[i - 1], heights[i]);
     }
 
     // rmax
     for (int i = n - 2; i >= 0; i--)
     {
-        rmax[i] = max(rmax[i+1], heights[i]);
+        rmax[i] = max(rmax[i + 1], heights[i]);
     }
-
 
     int ans = 0;
     for (int i = 0; i < n; i++)
@@ -33,10 +32,32 @@ int trap(vector<int> &heights)
     return ans;
 }
 
+// twopointer approach
+
+int trap2(vector<int> &heights)
+{
+    int n = heights.size(), l = 0, r = n - 1, ans = 0, lmax = 0, rmax = 0;
+
+    while(l < r){
+        lmax = max(lmax,heights[l]);
+        rmax = max(rmax, heights[r]);
+
+        if(lmax < rmax){
+            ans += lmax - heights[l];
+            l++;
+        }else{
+            ans += rmax - heights[r];
+            r--;
+        }
+    }
+    return ans;
+}
+
 int main()
 {
     vector<int> heights = {4, 2, 0, 3, 2, 5};
-    cout << trap(heights);
+    cout << trap(heights) << " ";
+    cout << trap2(heights);
 
     return 0;
 }
