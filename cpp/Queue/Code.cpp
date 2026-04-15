884af4fc684272f3fff610e7ea8267ef4a91f3dc
jitinkumarchak <jitin42013@gmail.com>
2026-04-15 23:11:51 +0530

    deque

--- cpp/Queue/Code.cpp
+++ cpp/Queue/Code.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <queue>
+#include <deque>
 using namespace std;
 
 int main()
 {
-   queue<int> q;
+    queue<int> q;
+    deque<int> dq;
 
     q.push(1);
     q.push(2);
     q.push(3);
+
+    dq.push_back(1);
+    dq.push_back(2);
+    dq.push_back(3);
+
+    dq.push_front(0);
+
+    while (!dq.empty())
+    {
+        cout << dq.front() << " ";
+        dq.pop_front();
+    }
+    cout << endl;
 
     while (!q.empty())
     {
