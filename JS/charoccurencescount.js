4d5ae72bfc1ecc85c7f2b70ba0e47db06b83d11b
jitinkumarchak <jitin42013@gmail.com>
2026-04-15 22:51:19 +0530

    Queue lec-77

---
+++ JS/charoccurencescount.js
@@ -0,0 +1,5 @@
+let name = "Jitin kumar chak";
+console.log(softwaredeveloper("name"));
+function softwaredeveloper(name) {
+  console.log(name);
+}
---
+++ code.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void software_developer(){
+    cout << "Jitin kumar chak" << endl;
+    cout << "lets Connect" << endl;
+}
+
+int main()
+{
+    software_developer();
+    return 0;
+}
