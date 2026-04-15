e07212cc434ed6cdd5f10c6d218c9df98e65ba42
jitinkumarchak <jitin42013@gmail.com>
2026-04-15 23:07:32 +0530

    Queue using C++ STL

--- cpp/Queue/Code.cpp
+++ cpp/Queue/Code.cpp
@@ -1,81 +1,10 @@
 #include <iostream>
 #include <queue>
-#include <list>
 using namespace std;
-
-class Node
-{
-public:
-    int data;
-    Node *next;
-
-    Node(int val)
-    {
-        data = val;
-        next = NULL;
-    }
-};
-
-class Queue
-{
-    Node *head;
-    Node *tail;
-
-public:
-    Queue()
-    {
-        head = tail = NULL;
-    }
-
-    void push(int data)
-    {
-        Node *newNode = new Node(data);
-
-        if (empty())
-        {
-            head = tail = newNode;
-        }
-        else
-        {
-            tail->next = newNode;
-            tail = newNode;
-        }
-    }
-
-    void pop()
-    {
-        if (empty())
-        {
-            cout << "Queue is empty";
-            return;
-        }
-        else
-        {
-            Node *temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
-
-    int front()
-    {
-        if (empty())
-        {
-            cout << "Queue is empty";
-            return -1;
-        }
-        return head->data;
-    }
-
-    bool empty()
-    {
-        return head == NULL;
-    }
-};
 
 int main()
 {
-    Queue q;
+   queue<int> q;
 
     q.push(1);
     q.push(2);
