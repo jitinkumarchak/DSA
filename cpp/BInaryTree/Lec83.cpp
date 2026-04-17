0c141a5a26213f3079218186fadba5da245b5b45
jitinkumarchak <jitin42013@gmail.com>
2026-04-17 19:18:15 +0530

    BUilding tree Using Recursion in Pre-order

--- cpp/BInaryTree/Lec83.cpp
+++ cpp/BInaryTree/Lec83.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Node
 {
+public:
     int data;
     Node *left;
     Node *right;
@@ -15,9 +16,28 @@ class Node
     }
 };
 
+static int idx = -1;
+Node *BuildTree(vector<int> preorder)
+{
+    idx++;
+    if (preorder[idx] == -1)
+    {
+        return NULL;
+    }
+
+    Node *root = new Node(preorder[idx]);
+    root->left = BuildTree(preorder);
+    root->right = BuildTree(preorder);
+
+    return root;
+}
+
 int main()
 {
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
-
+    Node *root = BuildTree(preorder);
+    cout << root->data << endl;
+    cout << root->left->data << endl;
+    cout << root->right->data << endl;
     return 0;
 }
