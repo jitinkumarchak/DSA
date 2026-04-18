3c3599dfa1fbd3f2d6d9770aa9be9cdde030ef06
jitinkumarchak <jitin42013@gmail.com>
2026-04-18 23:34:05 +0530

    LC:145 Post Order Traversal

---
+++ cpp/BInaryTree/LC145PostOrder.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+Node *BuildTree(vector<int> preorder)
+{
+    idx++;
+    if (preorder[idx] == -1)
+    {
+        return NULL;
+    }
+
+    Node *root = new Node(preorder[idx]);
+    root->left = BuildTree(preorder);
+    root->right = BuildTree(preorder);
+
+    return root;
+}
+
+void helper(Node *root, vector<int> &ans)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    helper(root->left, ans);
+    helper(root->right, ans);
+    ans.push_back(root->data);
+}
+
+int main()
+{
+    vector<int> Postorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
+    Node *root = BuildTree(Postorder);
+    vector<int> ans;
+    helper(root, ans);
+
+    for (int val : ans)
+    {
+        cout << val << " ";
+    }
+
+    return 0;
+}
