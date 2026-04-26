9a8b1902b33e2f3a35293e6c4f168f88d45647be
jitinkumarchak <jitin42013@gmail.com>
2026-04-27 00:44:16 +0530

    BellMan Ford  ALgo

---
+++ cpp/Graph/BellManFordAlgoDP.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <list>
+#include <stack>
+using namespace std;
+
+class Edge
+{
+public:
+    int v;
+    int wt;
+
+    Edge(int v, int wt)
+    {
+        this->v = v;
+        this->wt = wt;
+    }
+};
+// TC = O (( V+E).logV) , ElogE , ElogV, V^2LOGV
+// for directed graph = V(V-1) == V^2 = E
+// for undirected grapgh = V(V-1)/2
+
+void BellmanFord(int src, vector<vector<Edge>> g, int V)
+{
+    vector<int> dist(V, INT_MAX);
+    dist[src] = 0;
+
+    for (int i = 0; i < V - 1; i++)
+    {
+        for (int u = 0; u < V; u++) // u--->v
+        {
+            for (Edge e : g[u])
+            {
+                if (dist[e.v] > dist[u] + e.wt)
+                {
+                    dist[e.v] = dist[u] + e.wt;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < V; i++)
+    {
+        cout << dist[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int V = 5;
+
+    vector<vector<Edge>> g(V);
+
+    g[0].push_back(Edge(1, 2));
+    g[0].push_back(Edge(2, 4));
+    g[1].push_back(Edge(4, -1));
+    g[1].push_back(Edge(2, -4));
+    g[2].push_back(Edge(3, 2));
+    g[3].push_back(Edge(4, 4));
+
+    BellmanFord(0, g, V);
+    return 0;
+}
